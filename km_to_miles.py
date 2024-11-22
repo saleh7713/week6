03ad429357f28dafa0e67826fc2f98729d0ea0da
saleh7713 <sale7almrri0@gmail.com>
2024-11-22 03:08:42 +0100

    km to miles implementation

--- km_to_miles.py
+++ km_to_miles.py
@@ -1,5 +1,14 @@
 def km_to_miles(kilometers):
     # complete function implementation here...
 
+    miles_per_kilometer = 0.62
+
+    # Convert kilometers to miles
+    miles = kilometers * miles_per_kilometer
+
+    # rounding to 3 decimal places
+    miles = int(miles * 1000 + 0.5) / 1000.0
+
+    return miles
 
 print(km_to_miles(120)) 
