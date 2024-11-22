0b1df63d8356d00b42e4f065df4fad3a92a4699f
saleh7713 <sale7almrri0@gmail.com>
2024-11-22 03:09:08 +0100

    letter occurence implementation

--- letter_occurrence.py
+++ letter_occurrence.py
@@ -1,5 +1,14 @@
 def letter_occurrence(input_string):
     # complete function implementation...
+    count = 0
+
+    # Loop through each character in the string
+    for char in input_string:
+        # Compare the character with 'a' and 'A'
+        if char == 'a' or char == 'A':
+            count += 1
+
+    return count
 
 print(letter_occurrence("Amazing"))  # Expected output: 2
 print(letter_occurrence("Always aim ambitiously"))  # Expected output: 4
