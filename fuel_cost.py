a5b05a22d51076101c08d641404790446b01c8cf
saleh7713 <sale7almrri0@gmail.com>
2024-11-22 03:07:39 +0100

    fuel cost implementation

--- fuel_cost.py
+++ fuel_cost.py
@@ -4,6 +4,9 @@ def fuel_cost(distance_miles):
     LITERS_PER_GALLON = 4.5
     PRICE_PER_LITER = 1.49  # Price in pounds per liter
 #     continue function implementation here...
+    total_cost = PRICE_PER_LITER * distance_miles * LITERS_PER_GALLON / MPG
+    
+    return total_cost
 
 
 print(fuel_cost(50))  # Expected output: 6.705
